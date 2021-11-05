56bdb9f6a9ea6856d6ecfde1d3c6649d5ab8c461
BrendenESmith <bsmith11@babson.edu>
2021-11-04 21:24:33 -0400

    json function finished

--- mbta_helper.py
+++ mbta_helper.py
@@ -1,10 +1,20 @@
+import urllib.request
+import json
+from pprint import pprint
+
 # Useful URLs (you need to add the appropriate parameters for your requests)
 MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+MAPQUEST_API_KEY = "Ex93flIArrOmlQSA0ncpLwMPd9SlcVoO"
+MBTA_API_KEY = "51c6372aa69047a8b8c16271e877e968"
+
+url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College'
+f = urllib.request.urlopen(url)
+response_text = f.read().decode('utf-8')
+response_data = json.loads(response_text)
+pprint(response_data)
 
 
 # A little bit of scaffolding if you want to use it
@@ -14,7 +24,12 @@ def get_json(url):
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
+
+# get_json(f"http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College")
 
 
 def get_lat_long(place_name):
@@ -36,7 +51,7 @@ def get_nearest_station(latitude, longitude):
     """
     pass
 
-
+# Main function (the only funcion that needs to be imported to flask); combination of functions above
 def find_stop_near(place_name):
     """
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
