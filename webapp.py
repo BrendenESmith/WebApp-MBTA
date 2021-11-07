7be11b618ddba5551e4a1fb7d7dfbbb296459333
jleeeeee <77594057+jleeeeee@users.noreply.github.com>
2021-11-07 12:35:23 -0500

    webapp

--- webapp.py
+++ webapp.py
@@ -2,12 +2,14 @@
 Flask application for MBTA information. Given location as input, returns nearest MBTA station as output. 
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", method=["GET","POST"])
 def index():
+    if request.method == "POST":
+        address = float(request.form["address"])
     return render_template("index.html")
 
 
