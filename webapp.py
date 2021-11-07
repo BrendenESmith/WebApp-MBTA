ff525c4f5c9a2a83c7b015b0a76ef81823cb47af
jleeeeee <77594057+jleeeeee@users.noreply.github.com>
2021-11-07 14:06:07 -0500

    update

--- webapp.py
+++ webapp.py
@@ -2,14 +2,18 @@
 Flask application for MBTA information. Given location as input, returns nearest MBTA station as output. 
 """
 from flask import Flask, render_template, request
-
+from mbta_helper import get_json, find_stop_near
 app = Flask(__name__)
 
 @app.route("/", methods=['POST','GET'])
 def index():
     if request.method == 'POST':
-        if valid_address(request.form['address']):
-            return nearest_mbta(request.form['address'])
+        address = str(request.form["address"])
+        information = get_json(address)
+        if information:
+            return find_stop_near(render_template("result.html"),
+            station = information
+            )
         else: 
             return render_template("index.html", error=True)
     return render_template("index.html", error=None)
